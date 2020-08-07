b6b054b30b55b341c0bca1540d7a548ded0693e7
JWNicholson <john.nicholson812@gmail.com>
2020-08-07 13:23:09 -0400

    MVP and one extra function completed

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,28 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    #initalize empty list for results
+    result = []
+    #initialize a dictionary
+    directory = {}
+
+    for file in files:
+        #split to get the file name
+        split_path = file.split("/")
+        filename = split_path[-1]
+
+        #add to dict if not already in there
+        if filename not in directory:
+            directory[filename] = []
+
+            #add the current path using filename as key
+            directory[filename].append(file)
+
+        #iterate the queries param
+        for query in queries:
+            #if file is found add the path to result
+            if query in directory:
+                result.extend(directory[query])
 
     return result
 
@@ -23,3 +45,10 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
+
+
+#the filename is th last part of the path so split on / and get the last one
+#add to dict if needed
+#add current path to dict using filename as key
+#iterate through the queries, looking for the correct file
+#if file is found add the path to result. extend() adds the elements to the end of current list
